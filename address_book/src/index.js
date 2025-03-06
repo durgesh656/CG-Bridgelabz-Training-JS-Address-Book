2c50951a927cee42645bad73e47736d0d79a7176
durgesh656 <durgeshkushwaha656@gmail.com>
2025-03-07 00:37:31 +0530

    [Durgesh Kushwaha] Add: ability to find existing contact person using their name and edit it✨

--- address_book/src/index.js
+++ address_book/src/index.js
@@ -3,7 +3,6 @@ const Contact = require("./model/Contact.js");
 
 
 const myAddressBook = new AddressBook();
-
 try {
     const contact1 = new Contact("Durgesh", "Kushwaha", "Bhopal", "Madhya Pradesh", 462038, 9131733980, "durgesh123@gmail.com");
     myAddressBook.addContact(contact1);
--- address_book/src/service/AddressBook.js
+++ address_book/src/service/AddressBook.js
@@ -5,7 +5,7 @@ class AddressBook{
     constructor(){
         this.contacts = [];
     }
-
+    
     addContact = (contact)=>{
         if(!contact instanceof Contact){
             throw new Error(`Invalid contact ${contact}. Must be an instance of Contact`);
@@ -14,11 +14,33 @@ class AddressBook{
         this.contacts.push(contact);
     }
 
+      // Method to find the contact in the contact book using their name
+      findContact = (firstName, lastName) => {
+        return this.contacts.find(contact => contact.firstName === firstName && contact.lastName === lastName);
+    }
+
+    // Method to update the contact in the address book
+    editContact = (firstName, lastName, updatedData) => {
+        const contact = this.findContact(firstName, lastName);
+        if (!contact) {
+            throw new Error("Contact not found.");
+        }
+
+        Object.keys(updatedData).forEach(key => {
+            if (contact.hasOwnProperty(key)) {
+                contact[key] = updatedData[key];
+            }
+        });
+
+        return contact;
+    }
+
     // Method to get the contact
     getContact = ()=>{
         return this.contacts;
     }
 
+    
 
 }
 
